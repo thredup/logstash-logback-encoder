a9a914f3c07ea03edfe7799262e214153b878585
Max Bodaniuk <maksym.bodaniuk@thredup.com>
2019-06-07 02:59:07 +0300

    Add capability to name non-structured arguments

--- src/main/java/net/logstash/logback/composite/loggingevent/ArgumentsJsonProvider.java
+++ src/main/java/net/logstash/logback/composite/loggingevent/ArgumentsJsonProvider.java
@@ -11,44 +11,65 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.logstash.logback.composite.loggingevent;
 
-import java.io.IOException;
-
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.type.TypeReference;
 import net.logstash.logback.argument.StructuredArgument;
 import net.logstash.logback.composite.AbstractFieldJsonProvider;
 import net.logstash.logback.composite.FieldNamesAware;
+import net.logstash.logback.composite.JsonFactoryAware;
 import net.logstash.logback.fieldnames.LogstashFieldNames;
-import ch.qos.logback.classic.spi.ILoggingEvent;
 
-import com.fasterxml.jackson.core.JsonGenerator;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Include the logging event's {@link ILoggingEvent#getArgumentArray()} in the JSON output.
  * <p>
- *
+ * <p>
  * Arguments that are an instance of {@link StructuredArgument} will be output
  * as specified by {@link StructuredArgument#writeTo(JsonGenerator)}.
  * <p>
- * 
+ * <p>
  * Non-{@link StructuredArgument}s will be omitted unless {@link #includeNonStructuredArguments} is true.
  * When true, they will be included in the JSON output as separate fields
  * whose names are {@link #nonStructuredArgumentsFieldPrefix} plus the argument index.
  * (For example, "arg0").
  * </p>
- *
+ * <p>
  * If the fieldName is non-null, then the arguments will be written to that field as a subobject.
  * Otherwise, the arguments are written inline.
  */
-public class ArgumentsJsonProvider extends AbstractFieldJsonProvider<ILoggingEvent> implements FieldNamesAware<LogstashFieldNames> {
+public class ArgumentsJsonProvider extends AbstractFieldJsonProvider<ILoggingEvent> implements FieldNamesAware<LogstashFieldNames>, JsonFactoryAware {
 
     private boolean includeStructuredArguments = true;
     private boolean includeNonStructuredArguments;
     private String nonStructuredArgumentsFieldPrefix = "arg";
+    private String nonStructuredArgumentsFieldsMapping;
+    private JsonFactory jsonFactory;
+    private Map<String, String> nonStructuredArgumentsFieldsMappingMap = Collections.emptyMap();
 
     @Override
     public void writeTo(JsonGenerator generator, ILoggingEvent event) throws IOException {
-        
+
         if (!includeStructuredArguments && !includeNonStructuredArguments) {
             // Short-circuit if nothing is included
             return;
@@ -61,7 +82,7 @@ public class ArgumentsJsonProvider extends AbstractFieldJsonProvider<ILoggingEve
         }
 
         boolean hasWrittenFieldName = false;
-        
+
         for (int argIndex = 0; argIndex < args.length; argIndex++) {
 
             Object arg = args[argIndex];
@@ -81,7 +102,8 @@ public class ArgumentsJsonProvider extends AbstractFieldJsonProvider<ILoggingEve
                     hasWrittenFieldName = true;
                 }
                 String fieldName = nonStructuredArgumentsFieldPrefix + argIndex;
-                generator.writeObjectField(fieldName, arg);
+                String mappedName = nonStructuredArgumentsFieldsMappingMap.get(fieldName);
+                generator.writeObjectField(mappedName != null ? mappedName : fieldName, arg);
             }
         }
 
@@ -89,7 +111,7 @@ public class ArgumentsJsonProvider extends AbstractFieldJsonProvider<ILoggingEve
             generator.writeEndObject();
         }
     }
-    
+
     public boolean isIncludeStructuredArguments() {
         return includeStructuredArguments;
     }
@@ -97,7 +119,7 @@ public class ArgumentsJsonProvider extends AbstractFieldJsonProvider<ILoggingEve
     public void setIncludeStructuredArguments(boolean includeStructuredArguments) {
         this.includeStructuredArguments = includeStructuredArguments;
     }
-    
+
     public boolean isIncludeNonStructuredArguments() {
         return includeNonStructuredArguments;
     }
@@ -113,9 +135,36 @@ public class ArgumentsJsonProvider extends AbstractFieldJsonProvider<ILoggingEve
     public void setNonStructuredArgumentsFieldPrefix(String nonStructuredArgumentsFieldPrefix) {
         this.nonStructuredArgumentsFieldPrefix = nonStructuredArgumentsFieldPrefix;
     }
-    
+
+    public void setNonStructuredArgumentsFieldsMapping(String nonStructuredArgumentsFieldsMap) {
+        this.nonStructuredArgumentsFieldsMapping = nonStructuredArgumentsFieldsMap;
+        parseMapping();
+    }
+
+    public String getNonStructuredArgumentsFieldsMapping() {
+        return this.nonStructuredArgumentsFieldsMapping;
+    }
+
     @Override
     public void setFieldNames(LogstashFieldNames fieldNames) {
         setFieldName(fieldNames.getArguments());
+    }
+
+    @Override
+    public void setJsonFactory(JsonFactory jsonFactory) {
+        this.jsonFactory = jsonFactory;
+        parseMapping();
+    }
+
+    private void parseMapping() {
+        if (nonStructuredArgumentsFieldsMapping != null && this.jsonFactory != null) {
+            try {
+                nonStructuredArgumentsFieldsMappingMap = this.jsonFactory.
+                        createParser(this.nonStructuredArgumentsFieldsMapping).readValueAs(new TypeReference<HashMap<String, Object>>() {
+                });
+            } catch (IOException e) {
+                addError("Failed to parse custom fields [" + nonStructuredArgumentsFieldsMappingMap + "]", e);
+            }
+        }
     }
 }
